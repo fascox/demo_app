514fa554f34a42a70da1257eb720f3a058c86039
Fabio Scognamiglio <fascox@me.com>
2011-04-03 01:06:52 +0200

    Finish demo app

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -7,6 +7,7 @@ class MicropostsController < ApplicationController
     respond_to do |format|
       format.html # index.html.erb
       format.xml  { render :xml => @microposts }
+      format.json  { render :json => @microposts }
     end
   end
 
@@ -18,6 +19,7 @@ class MicropostsController < ApplicationController
     respond_to do |format|
       format.html # show.html.erb
       format.xml  { render :xml => @micropost }
+      format.json  { render :json => @micropost }
     end
   end
 
--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,2 +1,6 @@
 class Micropost < ActiveRecord::Base
+  
+  belongs_to :user
+  validates :content, :length => { :maximum => 30 }
+
 end
